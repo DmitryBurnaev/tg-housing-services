02f9b0ccfa7b907ecd8dcff484458b130184859f
DmitryBurnaev <dmitry.burnaev@gmail.com>
2024-05-20 09:45:37 +0300

    [#1] Parsing: add main handler's class

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import argparse
 
+from src.config.app import SupportedService, SupportedCity
+from src.parsing.main import Parser
+
 
 def main():
     parser = argparse.ArgumentParser(description="Process some addresses.")
     parser.add_argument('address', metavar='address', type=str)
     args = parser.parse_args()
-    print(args.address)
+    parser = Parser(city=SupportedCity.SPB, address=args.address)
+    for service in SupportedService.__members__:
+        result = parser.parse(service)
+        print(f"Parse Result: \n{result}")
 
 
 if __name__ == '__main__':
