eae7cc920614f00b6380796685e71d86d1ab4f9c
DmitryBurnaev <dmitry.burnaev@gmail.com>
2024-05-25 12:07:30 +0300

    [#1] Parsing: logging + small changes

--- src/parsing/main.py
+++ src/parsing/main.py
@@ -1,10 +1,14 @@
-from datetime import datetime, timezone, timedelta
+import logging
+import urllib.parse
 from typing import Any
+from datetime import datetime, timezone, timedelta
 
 import httpx
 from lxml import html
 
 from src.config.app import RESOURCE_URLS, SupportedCity, SupportedService, CITY_NAME_MAP
+
+logger = logging.getLogger(__name__)
 
 
 class Parser:
@@ -17,18 +21,19 @@ class Parser:
         self.finish_date = (datetime.now(timezone.utc) + timedelta(days=30)).date()
 
     def parse(self, service) -> dict[str, Any]:
-        print(f"Parsing for service: {service} ({self.address})")
+        logger.debug(f"Parsing for service: {service} ({self.address})")
         return self._parse_website(service)
 
     def _get_content(self, service: SupportedService) -> str:
-        city_name = CITY_NAME_MAP[self.city]
+        city_name = urllib.parse.quote_plus(CITY_NAME_MAP[self.city])
         url = self.urls[service].format(city=city_name, date_finish=self.finish_date.isoformat())
+        logger.debug("Getting content for service: %s ...", url)
         with httpx.Client() as client:
             response = client.get(url)
 
         return response.text
 
-    def _parse_website(self, service: SupportedService) -> dict[str, Any]:
+    def _parse_website(self, service: SupportedService) -> dict[str, Any] | None:
         """
         Parses websites by URL's provided in params
 
@@ -41,6 +46,9 @@ class Parser:
         tree = html.fromstring(html_content)
         rows = tree.xpath("//table/tr")
         data = []
+        if not rows:
+            logger.info("No data found for service: %s", service)
+            return None
 
         for row in rows:
             columns = row.xpath("td/text()")
