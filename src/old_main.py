efab850020c726c2ceea44958bec499c6bdb5c6c
DmitryBurnaev <dmitry.burnaev@gmail.com>
2024-06-21 10:11:46 +0300

    [#3] TG-integration: added main and tests handlers

---
+++ src/old_main.py
@@ -0,0 +1,34 @@
+import argparse
+import logging.config
+import uuid
+
+from src.config.app import SupportedService, SupportedCity
+from src.config.logging import LOGGING_CONFIG
+from src.db.models import User
+from src.parsing.main import Parser
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Process some addresses.")
+    parser.add_argument("address", metavar="address", type=str)
+    logging.config.dictConfig(LOGGING_CONFIG)
+    logging.captureWarnings(capture=True)
+
+    args = parser.parse_args()
+
+    user = User(
+        id=uuid.uuid4(),
+        name="TestUser",
+        city=SupportedCity.SPB,
+        raw_address=args.address,
+    )
+    service_data_parser = Parser(city=user.address.city)
+    result = service_data_parser.parse(SupportedService.ELECTRICITY, user_address=user.address)
+    logger.info(f"Parse Result: \n{result}")
+    user.send_notification(result)
+
+
+if __name__ == "__main__":
+    main()
