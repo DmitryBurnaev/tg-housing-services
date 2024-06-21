5c35d3cd8e1f742b46a9458b4deb754288c82393
DmitryBurnaev <dmitry.burnaev@gmail.com>
2024-06-21 10:11:46 +0300

    [#3] TG-integration: added main and tests handlers

--- src/main.py
+++ src/main.py
@@ -1,34 +1,54 @@
-import argparse
-import logging.config
-import uuid
+import asyncio
+import logging
+import sys
 
-from src.config.app import SupportedService, SupportedCity
-from src.config.logging import LOGGING_CONFIG
-from src.db.models import User
-from src.parsing.main import Parser
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
 
+from src.config.app import TG_BOT_API_TOKEN
+
+dp = Dispatcher()
 logger = logging.getLogger(__name__)
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Process some addresses.")
-    parser.add_argument("address", metavar="address", type=str)
-    logging.config.dictConfig(LOGGING_CONFIG)
-    logging.captureWarnings(capture=True)
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/start` command
+    """
+    await message.answer(f"Hi, {html.bold(message.from_user.full_name)}!")
 
-    args = parser.parse_args()
 
-    user = User(
-        id=uuid.uuid4(),
-        name="TestUser",
-        city=SupportedCity.SPB,
-        raw_address=args.address,
-    )
-    service_data_parser = Parser(city=user.address.city)
-    result = service_data_parser.parse(SupportedService.ELECTRICITY, user_address=user.address)
-    logger.info(f"Parse Result: \n{result}")
-    user.send_notification(result)
+@dp.message(Command("address"))
+async def command_address_handler(message: Message) -> None:
+    """
+    This handler receives messages with `/address` command
+    """
+    await message.answer(f"Set your address, {html.bold(message.from_user.full_name)}!")
+
+
+@dp.message()
+async def echo_handler(message: Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        print(f"Got message {message.chat.id=} | {message}")
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    bot = Bot(token=TG_BOT_API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
